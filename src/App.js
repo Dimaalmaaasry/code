8190c612489a77b88331bca011adf911d2e3f94e
Dimaalmaaasry <moeen.wm.8@gmail.com>
2021-01-15 18:50:31 +0200

    first

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const css = {
+    height: 120px;
+    width: 750px;
+	  border-radius: 3%;
+	  background-color: white;
+	  opacity: 0.69;
+	  margin-left: 300px;
+  };
+  return (
+    <div id = "header">
+          <h1 style={css}> fashion</h1>
+    </div>
+
+
+
+
+
+
+
+
+
+  );
+}
+
+export default App;
